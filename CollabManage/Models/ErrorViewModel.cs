f4cac162f5597c6a1bb2bc597d650a9d4ed944f0
Rafael Menezes <rafa.rcm@hotmail.com>
2024-06-20 09:06:07 -0300

    Add project files.

---
+++ CollabManage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CollabManage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
