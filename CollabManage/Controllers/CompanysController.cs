a7e54044a7a0c09617e606e4facb9ba690511328
Rafael Menezes <rafa.rcm@hotmail.com>
2024-06-20 21:28:08 -0300

    Adicionando Controller/View Company

---
+++ CollabManage/Controllers/CompanysController.cs
@@ -0,0 +1,20 @@
+﻿using CollabManage.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollabManage.Controllers
+{
+    public class CompanysController : Controller
+    {
+        private readonly CollabManageContext _context;
+
+        public CompanysController(CollabManageContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Company.ToListAsync());
+        }
+    }
+}
